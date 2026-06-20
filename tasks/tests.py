ec96f1516220a39ae50efa4d8ae6732d65e0361d
Martyna <c8d5tnrz8s@privaterelay.appleid.com>
2026-06-20 15:56:12 +0200

    Update tests.py

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from .models import Subject, Student, Homework
+from datetime import date
 
-# Create your tests here.
+
+class HomeworkModelTest(TestCase):
+
+    def test_create_homework(self):
+
+        subject = Subject.objects.create(
+            name="Informatyka"
+        )
+
+        student = Student.objects.create(
+            first_name="Jan",
+            last_name="Kowalski"
+        )
+
+        homework = Homework.objects.create(
+            description="Przygotować prezentację",
+            due_date=date.today(),
+            status="TODO",
+            subject=subject,
+            student=student
+        )
+
+        self.assertEqual(
+            homework.description,
+            "Przygotować prezentację"
+        )
